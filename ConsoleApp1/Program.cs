b1ab3ff9ce13bbc3bc49b3bea706ccb20061dd5d
Ludvig Viveland <ludvigvive@gmail.com>
2020-10-14 12:57:36 +0200

    change

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int items = 10;
+            int totalSum = 0;
+            int numberofSixes = 0;
+
+            Random rnd = new Random();
+
+            int[] randomNumbers = new int[items];
+
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+                Console.Write("Ange ett tal från 1 till och med 6:");
+
+                    userInput = Console.ReadLine();
+                randomNumbers[i] = int.Parse(userInout);
+
+                for (int i = 0; i < randomNumbers.Length; i++)
+                {
+
+
+                }
+
+
+            }
+  
+            Console.WriteLine("Summan av talen är" + totalSum);
+        }
+    }
+}
