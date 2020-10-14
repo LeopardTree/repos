e30d658d14d2cfaff7d345b4df218f9b1d68bb10
Ludvig Viveland <ludvigvive@gmail.com>
2020-10-14 15:27:55 +0200

    2

--- ast2_1/Program.cs
+++ ast2_1/Program.cs
@@ -14,12 +14,14 @@ namespace ast2_1
 
             double circumference = Math.PI * userInput;
 
-            Console.WriteLine("omkretsen: {0}", circumference);
+            Console.WriteLine("omkretsen: {0}", circumference.ToString("F3"));
 
             Console.WriteLine("En cirkel med diametern {0} har: ", userInput);
 
+            //Console.WriteLine("... omkretsen: " + perimeter.ToString("F2"));
+
             double area = Math.PI * userInput / 2;
-            Console.WriteLine("arean: {0}", area);
+            Console.WriteLine("arean: {0}", area.ToString("F3"));
 
             Console.ReadKey();
         }
