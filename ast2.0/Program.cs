a1e1a59a9103ae8783da435057277ae3a7530b97
Ludvig Viveland <ludvigvive@gmail.com>
2020-10-15 11:09:23 +0200

    done

---
+++ ast2.0/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ast2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad kör du i för hastighet?:");
+            int speed = int.Parse(Console.ReadLine());
+
+            if (speed <= 30)
+            {
+                Console.WriteLine("Du kör lagligt!");
+            }
+            else
+            {
+
+                if (speed <= 40)
+                {
+                    Console.WriteLine("Du kör för fort och får" +
+                        "böta 2000 SEK");
+                }
+
+                else
+                {
+
+
+
+                    if (speed <= 50)
+                    {
+                        Console.WriteLine("Du har kört för fort" +
+                            "och får böta 3000 SEK");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Du har kört för fort " +
+                            "och får böta 5000 SEK!" +
+                            "Dessutom blir du av med körkortet.");
+                    }
+                }
+
+            }
+            Console.ReadKey();
+        }
+
+                
+                //userInput = userInput.ToString("F3");
+            
+        
+    }
+}
